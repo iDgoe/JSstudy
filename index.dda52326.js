be482f07afc592058b107459f914c364e97a9ca4
iDgoe <122863422+iDgoe@users.noreply.github.com>
2023-05-29 18:37:29 +0000

    Deploying to gh-pages from @ iDgoe/JSstudy@7b4f1bc20afaab0154f342eb45d10e946aea9b42 🚀

---
+++ index.dda52326.js
@@ -0,0 +1,2 @@
+const t=[{title:"The Last Kingdom",author:"Bernard Cornwell",rating:8.38},{title:"На березі спокійних вод",author:"Роберт Шеклі",rating:8.51},{title:"Сон смішної людини",author:"Федір Достоєвський",rating:7.75}];for(const o of t)console.log(o),console.log(o.title);
+//# sourceMappingURL=index.dda52326.js.map
