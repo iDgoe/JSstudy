85ddf162bfbbcabe8f3a71caab9d923e06aed234
iDgoe <122863422+iDgoe@users.noreply.github.com>
2023-06-19 07:53:36 +0000

    Deploying to gh-pages from @ iDgoe/JSstudy@674f648d5b5a744ce195b486f028fb812c3f7d81 🚀

--- index.2a73ca67.js
+++
@@ -1,2 +0,0 @@
-[5,10,15,20,25].forEach(((o,c)=>{console.log(`Індекс ${c}, значення ${o}`)}));
-//# sourceMappingURL=index.2a73ca67.js.map
--- index.3f753d55.js
+++
@@ -1,2 +0,0 @@
-[5,10,15,20,25].forEach((function(c,o){console.log("Індекс ".concat(o,", значення ").concat(c))}));
-//# sourceMappingURL=index.3f753d55.js.map
