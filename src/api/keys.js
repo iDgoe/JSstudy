1f3259edb8ccca0f9781d56a259f259010a8d08f
iDgoe <e.novykov@gmail.com>
2023-06-25 15:42:59 +0300

    25.06

---
+++ src/api/keys.js
@@ -0,0 +1,34 @@
+
+  const keys = [{
+    publickey: 'caba337208b47569b1f84f369984c4a2',
+    privatekey: 'b496be0246e5deafeb43afa6abcf6a04e1453022',
+  },
+  {
+    publickey: '2d66dc4d241b1e725a65709fbddb3fff',
+    privatekey: '7bcdbaf715b35f3ca9f820b168f7904a5457f407',
+  },
+  {
+    publickey: '2948b8e0caf400a2826fafe939e0b4f9',
+    privatekey: 'cd4405cc83dbf1a338db18b1461414480be3621f',
+  },
+
+  {
+    publickey: '10d9adf698c7e03e826886b469f64fda',
+    privatekey: 'bebc14fb8e0de0eb1ddaa7489aa94e51c83d5ba9',
+  },
+  {
+    publickey: 'f9437ef5f8096b58a2a964f8fad9d2c4',
+    privatekey: '927ea36362f0f7d499872f2aeb3575e8c79efc03',
+  },
+
+  {
+    publickey: '8d0b3a7388f5897b96460142d09632e5',
+    privatekey: '76942b2e499cc9bbd95edf77bf1b41d36b109cdb',
+  },
+  {
+    publickey: 'f9437ef5f8096b58a2a964f8fad9d2c4',
+    privatekey: '927ea36362f0f7d499872f2aeb3575e8c79efc03',
+  },
+];
+
+export const key = keys[Math.round(Math.random() * (keys.length - 1))];
